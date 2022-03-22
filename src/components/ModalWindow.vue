00dafc6a55d26ce703a8d86b82a28cc9c7eec30e
Srisaravana Manicaraja <saravanamuthaly@gmail.com>
2022-03-22 07:46:37 +0530

    updated modal to have below footer area for error messages

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,60 @@
+<template>
+
+  <input type="checkbox" :id="id" class="modal-toggle" v-model="checkBox">
+  <div class="modal">
+    <div class="modal-box relative">
+      <label class="btn btn-sm btn-circle absolute right-2 top-2" @click="handleClose">
+        ✕
+      </label>
+      <section>
+
+        <header class="text-lg font-bold uppercase">
+          <slot name="header">Modal title</slot>
+        </header>
+
+        <main class="py-2">
+          <slot name="main">modal body</slot>
+        </main>
+
+        <footer class="gap-2 flex justify-end mt-5">
+          <slot name="footer"/>
+        </footer>
+
+        <footer class="gap-2 flex justify-center mt-5">
+          <slot name="belowFooter"/>
+        </footer>
+
+      </section>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+
+import {ref, watchEffect} from 'vue';
+
+const emits = defineEmits(['closed']);
+
+const props = defineProps({
+  id: String,
+  class: String,
+  visible: Boolean
+});
+
+const checkBox = ref(false);
+
+watchEffect(() => {
+  checkBox.value = props.visible;
+});
+
+
+function handleClose() {
+  emits('closed');
+}
+
+</script>
+
+<style scoped>
+
+</style>
