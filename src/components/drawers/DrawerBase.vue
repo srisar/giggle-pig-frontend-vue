60f2e1f6d4b44fd99912d55efe4a46cef49f0200
Srisaravana Manicaraja <saravanamuthaly@gmail.com>
2022-03-22 07:47:59 +0530

    added CommonDrawer.vue

---
+++ src/components/drawers/DrawerBase.vue
@@ -0,0 +1,25 @@
+<!--suppress ALL -->
+<template>
+
+  <div class="drawer-side" style="scroll-behavior: smooth; scroll-padding-top: 5rem">
+    <label for="my-drawer" class="drawer-overlay"></label>
+    <aside class="overflow-y-auto w-64 bg-base-300 text-base-content">
+
+      <div class="h-4"></div>
+
+      <slot/>
+
+    </aside>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'DrawerBase'
+};
+</script>
+
+<style scoped>
+
+</style>
